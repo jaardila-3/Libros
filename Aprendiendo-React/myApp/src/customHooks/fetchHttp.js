f0962e2316fdb4f89b516ae2531ef98bc53ff3ca
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-09-24 12:20:59 -0500

    guardando el fichero fetchHttp.js que es ell custom hook para las peticiones HTTP y que no fue guardado por el comnado git commit -am

---
+++ Aprendiendo-React/myApp/src/customHooks/fetchHttp.js
@@ -0,0 +1,33 @@
+import { useCallback, useState } from "react"
+
+export const useHttp = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+
+    const sendRequest = useCallback(
+        async (url, method = 'GET', body = null) => {
+            setIsLoading(true);
+            setError(null);
+            //setData(null);
+            try {
+                const response = await fetch(url, {
+                    method,
+                    body: body ? JSON.stringify(body) : null,
+                    headers: body ? { 'Content-Type': 'application/json' } : {}
+                });
+
+                if (!response.ok) {
+                    throw new Error('Request failed!');
+                }
+
+                const data = await response.json();
+                setData(data);
+            } catch (error) {
+                setError(error.message || 'Something went wrong!');
+            }
+            setIsLoading(false);
+        }, []
+    );
+    return { isLoading, error, data, sendRequest };
+}
